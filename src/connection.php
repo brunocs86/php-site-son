045dda03a794b19c2f19332e3fa4f4619cf98ac9
Bruno Silva <brunosilva@programacaolivre.com.br>
2019-04-21 00:31:51 -0300

    alteracao

--- src/connection.php
+++ src/connection.php
@@ -3,9 +3,3 @@
 mysqli_report(MYSQLI_REPORT_ERROR);
 
 $conn = new mysqli('localhost', 'bcs', '123', 'php_son_admin');
-
-/*if ($conn->connect_errno) {
-    die('Falha de conexao ao MySQL: ('.$conn->connection_errno.') '.$conn->connection_error);
-}
-
-return $conn;*/
